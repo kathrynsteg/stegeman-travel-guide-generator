cdac04c2d6e90b4e8302fde2aafe71ae8e1e532a
kathrynsteg <kathrynsteg@gmail.com>
2024-01-28 20:56:49 -0600

    Added API integration and form functionality

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
+function displayGuide(response) {
+  new Typewriter("#guide", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 20,
+    cursor: "",
+  });
+}
+
 function generateGuide(event) {
   event.preventDefault();
 
-  new Typewriter("#guide", {
-    strings: "Now generating guide...",
-    autoStart: true,
-    delay: 15,
-    cursor: "",
-  });
+  // build API URL
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "15aa7ff71012241631cb1665815teob0";
+  let prompt = `User Instructions: Generate a concise travel guide with the 5 best things to do as a tourist in ${instructionsInput.value}.`;
+  let context =
+    "You are an expert travel guide and love cultural sites and local cuisine. Your mission is to generate a 5 line travel guide in basic HTML. Generate the title as an <h1> and all remaining text as <p> separating each line with a <br />. Make sure to use the User Instructions. Sign guide at the end with <strong>xoxo, SheCodes AI ❤</strong>";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  // call to API
+  axios.get(apiUrl).then(displayGuide);
 }
 
 let guideFormElement = document.querySelector("#guide-generator");
